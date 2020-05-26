cfe3d443077bdf564ad126291722d365d5a355ca
Krasivaya <semwagacarine@gmail.com>
2020-05-26 20:55:49 +0200

    Add article route

--- connect/urls.py
+++ connect/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns=[
-    url(r'', views.posts, name='all_posts'),
+    url(r'^$', views.posts, name='posts'),
+    url(r'^article/(\d+)', views.article, name='article'),
 ]
