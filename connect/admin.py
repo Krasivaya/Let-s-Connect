548fe256662e32d6c5f6c29e1bc6e7bc07b25122
Krasivaya <semwagacarine@gmail.com>
2020-05-26 20:21:00 +0200

    Add models registration

--- connect/admin.py
+++ connect/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Author, Post
 
-# Register your models here.
+# Registering Models to Admin.
+admin.site.register(Author)
+admin.site.register(Post)
