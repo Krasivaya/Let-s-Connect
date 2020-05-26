d3b1701aa32f8c4f58be496fe0bac7b6998359bc
Krasivaya <semwagacarine@gmail.com>
2020-05-26 20:21:25 +0200

    Add post model class

--- connect/models.py
+++ connect/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Author Model Class
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+# Post Model Class
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.title
