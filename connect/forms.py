8481e309dfde51ee7f4044a7e1000dacfe6ec679
Krasivaya <semwagacarine@gmail.com>
2020-05-27 22:48:16 +0200

    Add initial comment field widgts

--- connect/forms.py
+++ connect/forms.py
@@ -3,3 +3,7 @@ from .models import Comment
 
 # Comment form class
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add comment...'
